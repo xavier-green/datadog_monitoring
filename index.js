ba6f9378e94ff48ec2e4ba533290805e5f67757b
Green <xggreen@amazon.fr>
2017-11-05 13:35:16 +0100

    Adding initial prompt to add websites

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
-const default_websites = require('./src/config/default_websites'),
-      dbInit = require('./src/db/db_functions').init,
+const dbInit = require('./src/db/db_functions').init,
       treatWebsiteQueue = require('./src/ping/request').treatWebsiteQueue,
       fs = require("fs");
 
 const console_run = require('./src/client/console');
 
-dbInit(default_websites);
-console.log("\nStarted");
-treatWebsiteQueue(default_websites);
-console.log("Now getting stats");
+var stdin = process.openStdin(),
+    default_websites = require('./src/config/default_websites');
 
-setTimeout(function () {
-    console_run();
-}, 5000);
+console.log("\nSome default websites have been entered: google.com / datadog.com / tobby.io / sdkglhsdgksdhgklsdh.com");
+console.log("\nIf you want to enter some additional ones, please enter them here");
+console.log("\nTo start running the program, please type 'run'\n\n");
+stdin.addListener("data", function(d) {
+    let user_input = d.toString().trim();
+    if (user_input === "run") {
+        dbInit(default_websites);
+        console.log("\nEntering the websites in the database");
+        treatWebsiteQueue(default_websites);
+        console.log("\nStarting the program");
+        setTimeout(function () {
+            console_run();
+        }, 5000);
+    } else {
+        if (checkURL(user_input)) {
+            default_websites.push({
+                url: user_input,
+                interval: 3000
+            })
+            console.log(user_input,"added\n");
+            console.log("Please enter a new website or type 'run'\n");
+        } else {
+            console.log("\nInvalid website, please add one of the form [http(s)]://[domain.com]([:port])\n");
+        }
+    }
+  });
+
+checkURL = (str) => {
+  var pattern = /(http|https):\/\/(\w+:{0,1}\w*)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/;
+  if(!pattern.test(str)) {
+    return false;
+  } else {
+    return true;
+  }
+}
